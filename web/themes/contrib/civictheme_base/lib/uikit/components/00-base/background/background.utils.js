51d56bade0d844a4e7cd257225be0556295616d1
Richard Gaunt <richard.gaunt@protonmail.com>
2024-12-31 11:31:49 +1100

    Add new storybook.

--- web/themes/contrib/civictheme_base/lib/uikit/components/00-base/background/background.utils.js
+++ web/themes/contrib/civictheme_base/lib/uikit/components/00-base/background/background.utils.js
@@ -4,11 +4,11 @@
  * Background component utilities.
  */
 
-const fs = require('fs');
-const pathUtil = require('path');
+import fs from 'fs';
+import pathUtil from 'path';
 
 const dir = '../../../assets/backgrounds';
-const basePath = pathUtil.resolve(__dirname, dir);
+const basePath = pathUtil.resolve(import.meta.dirname, dir);
 const paths = fs.readdirSync(basePath);
 
 function getBackgrounds() {
@@ -19,6 +19,6 @@ function getBackgrounds() {
   return urls;
 }
 
-module.exports = {
-  getBackgrounds,
-};
+export default {
+  getBackgrounds
+}
